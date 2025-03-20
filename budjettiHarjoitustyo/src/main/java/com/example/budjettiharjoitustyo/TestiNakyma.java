205245fd1156024161717538256b2f733a6257f8
Aino Komulainen <ainokaroliinakomulainen@gmail.com>
2025-03-20 09:31:42 +0200

    näkymämuutoksia

--- budjettiHarjoitustyo/src/main/java/com/example/budjettiharjoitustyo/TestiNakyma.java
+++ budjettiHarjoitustyo/src/main/java/com/example/budjettiharjoitustyo/TestiNakyma.java
@@ -6,6 +6,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -23,30 +24,51 @@ public class TestiNakyma extends Application {
         root.setPadding(new Insets(10,10,10,10));
 
         Label paaTeksti=new Label("Kuukauden budjettisi:");
+        StackPane ansioYmpyraPaneeli =new StackPane();
+        StackPane etuusYmpyraPaneeli=new StackPane();
+        StackPane tuloYmpyraPaneeli=new StackPane();
         
 
 
         //tuloille
-        VBox tuloLaatikko=new VBox();
         Circle ansioYmpyra=new Circle(40);
-        Circle etuusYmypyra=new Circle(40);
-        Circle muuTuloYmpyra=new Circle(40);
+        Label ansiolb=new Label("Ansiot");
+        ansioYmpyraPaneeli.getChildren().addAll(ansioYmpyra,ansiolb);
         ansioYmpyra.setFill(Color.AQUAMARINE);
+        Circle etuusYmypyra=new Circle(40);
+        Label etuuslb=new Label("Etuudet");
+        etuusYmpyraPaneeli.getChildren().addAll(etuusYmypyra,etuuslb);
         etuusYmypyra.setFill(Color.AZURE);
+        Circle muuTuloYmpyra=new Circle(40);
+        Label muuTulolb=new Label("Muut tulot");
+        tuloYmpyraPaneeli.getChildren().addAll(muuTuloYmpyra,muuTulolb);
         muuTuloYmpyra.setFill(Color.AQUA);
-        tuloLaatikko.getChildren().addAll(ansioYmpyra,etuusYmypyra,muuTuloYmpyra);
+
+
+        VBox tuloLaatikko=new VBox();
+        tuloLaatikko.getChildren().addAll(ansioYmpyraPaneeli,etuusYmpyraPaneeli,tuloYmpyraPaneeli);
         tuloLaatikko.setAlignment(Pos.BOTTOM_RIGHT);
         tuloLaatikko.setSpacing(10);
 
         //menooille sama
-        VBox menoLaatikko=new VBox();
+        StackPane vuokraYmpyraPaneeli =new StackPane();
+        StackPane ruokaYmpyraPaneeli=new StackPane();
+        StackPane menoYmpyraPaneeli=new StackPane();
         Circle vuokraYmpyra=new Circle(40);
-        Circle ruokaYmpyra=new Circle(40);
-        Circle muuMenoYmpyra=new Circle(40);
+        Label vuokralb=new Label("Vuokra");
         vuokraYmpyra.setFill(Color.FUCHSIA);
+        vuokraYmpyraPaneeli.getChildren().addAll(vuokraYmpyra,vuokralb);
+        Circle ruokaYmpyra=new Circle(40);
+        Label ruokalb=new Label("Ruoka");
         ruokaYmpyra.setFill(Color.FIREBRICK);
+        ruokaYmpyraPaneeli.getChildren().addAll(ruokaYmpyra,ruokalb);
+        Circle muuMenoYmpyra=new Circle(40);
+        Label menolb=new Label("Muut menot");
         muuMenoYmpyra.setFill(Color.RED);
-        menoLaatikko.getChildren().addAll(vuokraYmpyra,ruokaYmpyra,muuMenoYmpyra);
+        menoYmpyraPaneeli.getChildren().addAll(muuMenoYmpyra,menolb);
+
+        VBox menoLaatikko=new VBox();
+        menoLaatikko.getChildren().addAll(vuokraYmpyraPaneeli,ruokaYmpyraPaneeli,menoYmpyraPaneeli);
         menoLaatikko.setAlignment(Pos.BOTTOM_LEFT);
         menoLaatikko.setSpacing(10);
 
